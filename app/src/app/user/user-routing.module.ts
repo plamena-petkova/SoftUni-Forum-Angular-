d493076e7480b458c7b255242049eab6bbec6729
Plamena Petkova <pl.petkova@gmail.com>
2022-03-17 16:46:06 +0200

    Added guard

--- app/src/app/user/user-routing.module.ts
+++ app/src/app/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginGuard } from "../login.guard";
 import { LoginComponent } from "./login/login.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { RegisterComponent } from "./register/register.component";
@@ -7,6 +8,7 @@ import { RegisterComponent } from "./register/register.component";
 const routes: Routes = [
     {
         path: 'login',
+        canActivate: [LoginGuard],
         component: LoginComponent
     },
     {
